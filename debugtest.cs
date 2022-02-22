45f65355fc3a24dfeb1625c3959fcc8a559b5994
Terence1k <99253320+Terence1k@users.noreply.github.com>
2022-02-22 11:26:32 +0100

    Create debugtest.cs

---
+++ debugtest.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        string s = "";
+        for (int i = 5; i <= 10; i += 2)
+            s += i + " ";
+
+        for (int i = 10; i >= 3; i--)
+            if (i % 2 == 0)
+                s += i + " ";
+
+        s = "";
+        for (int i = 7; i <= 14; i += 3)
+            s += i + " ";
+        s += "!";
+
+        int sum = 0;
+        for (int apple = 1; apple <= 3;)
+            sum = sum + apple++;
+    }
+}
